846de22dadbb6dad7800cab4eb1eec3d996f3457
ask4fusora <theswe@ask4fusora.dev>
2026-08-23 15:03:37 +0700

    Configure osc52 copying

--- lua/ssh/setup.lua
+++ lua/ssh/setup.lua
@@ -2,11 +2,4 @@ if not vim.env.SSH_CONNECTION then
     return
 end
 
-local osc52_clipboard = require("vim.ui.clipboard.osc52")
-
-vim.g.clipboard = {
-    copy = {
-        ["+"] = osc52_clipboard.copy("+"),
-        ["*"] = osc52_clipboard.copy("*"),
-    },
-}
+vim.g.clipboard = "osc52"
