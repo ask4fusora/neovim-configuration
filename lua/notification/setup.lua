cd14aa62b2be96ec6e8255d41be8c5c4e366ab7c
ask4fusora <theswe@ask4fusora.dev>
2026-08-27 00:08:41 +0700

    feat(notification): simplify `mini.notify` setup options

--- lua/notification/setup.lua
+++ lua/notification/setup.lua
@@ -1,21 +1,10 @@
 local success, mn = pcall(require, "mini.notify")
 if not success then
-    vim.notify("`mini.notify` is either not installed or not available.", vim.log.levels.ERROR)
+    vim.notify(
+        "`mini.notify` is either not installed or not available.",
+        vim.log.levels.ERROR
+    )
     return
 end
 
-mn.setup({
-    window = { max_width_share = 0.618 },
-    content = {
-        format = function(notification)
-            local time_string =
-                vim.fn.strftime("%H:%M:%S", math.floor(notification.ts_update))
-
-            return (" %s | %s | %s "):format(
-                time_string,
-                notification.level,
-                notification.msg
-            )
-        end,
-    },
-})
+mn.setup()
