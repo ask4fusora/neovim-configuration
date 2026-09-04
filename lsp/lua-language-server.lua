b70326c170ae8019acd4d668d4e7db38631be952
ask4fusora <theswe@ask4fusora.dev>
2026-09-05 00:22:24 +0700

    feat(util): rename `lua` module to `util`

--- lsp/lua-language-server.lua
+++ lsp/lua-language-server.lua
@@ -47,7 +47,7 @@ return {
 
         local user_settings_dir = vim.fn.stdpath("config")
         local is_in_user_settings_dir =
-            require("lua.string").starts_with(buffer_path, user_settings_dir)
+            require("util.string").starts_with(buffer_path, user_settings_dir)
 
         if is_in_user_settings_dir then
             return on_dir(user_settings_dir)
@@ -77,7 +77,7 @@ return {
         ---Project settings folder name.
         local searchexpr = ("/%s/"):format(dotnvim)
         local is_in_project_settings_dir = root_dir:find(searchexpr, 1, true)
-        local is_in_user_settings_dir = require("lua.string").starts_with(
+        local is_in_user_settings_dir = require("util.string").starts_with(
             root_dir,
             vim.fn.stdpath("config")
         )
