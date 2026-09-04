eeb32301ce0e28a5b08707c9f50cd5abe8075db9
ask4fusora <theswe@ask4fusora.dev>
2026-09-05 00:33:40 +0700

    fix(lsp): replace path-containing logic

--- lsp/lua-language-server.lua
+++ lsp/lua-language-server.lua
@@ -31,9 +31,9 @@ return {
     },
 
     root_dir = function(bufnr, on_dir)
-        local searchexpr = ("/%s/"):format(dotnvim)
         local buffer_path = vim.api.nvim_buf_get_name(bufnr)
-        local is_in_project_settings_dir = buffer_path:find(searchexpr, 1, true)
+        local is_in_project_settings_dir =
+            require("util.path").contains_folder(buffer_path, dotnvim)
 
         if is_in_project_settings_dir then
             local cwd = vim.fs.root(bufnr, { dotnvim })
@@ -74,9 +74,8 @@ return {
             return
         end
 
-        ---Project settings folder name.
-        local searchexpr = ("/%s/"):format(dotnvim)
-        local is_in_project_settings_dir = root_dir:find(searchexpr, 1, true)
+        local is_in_project_settings_dir =
+            require("util.path").contains_folder(root_dir, dotnvim)
         local is_in_user_settings_dir = require("util.string").starts_with(
             root_dir,
             vim.fn.stdpath("config")
