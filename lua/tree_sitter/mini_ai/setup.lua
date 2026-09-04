7a9f7eadd95ae6b6e9bd6818d0f0a7e5ae6ffe68
ask4fusora <theswe@ask4fusora.dev>
2026-09-05 00:06:03 +0700

    style: run stylua format

--- lua/tree_sitter/mini_ai/setup.lua
+++ lua/tree_sitter/mini_ai/setup.lua
@@ -1,6 +1,9 @@
 local success, mini_ai = pcall(require, "mini.ai")
 if not success then
-    vim.notify("`mini.ai` is either not installed or not available.", vim.log.levels.ERROR)
+    vim.notify(
+        "`mini.ai` is either not installed or not available.",
+        vim.log.levels.ERROR
+    )
     return
 end
 
