ec62a091921d038504cea6f287d2ef1718c882a4
ask4fusora <theswe@ask4fusora.dev>
2026-09-06 19:24:01 +0700

    refactor(formatter): modulize format calls

--- lua/formatter/init.lua
+++ lua/formatter/init.lua
@@ -11,40 +11,95 @@ local function default_formatters()
         :totable()
 end
 
----@return string[] lines
+---@param formatter fsr.formatter.Formatter.LanguageServer
+local function format_with_language_server(formatter)
+    vim.lsp.buf.format({
+        filter = function(client)
+            return client.name == formatter.language_server.name
+        end,
+    })
+end
+
 ---@return integer start_linenr 0-indexed.
----@return integer end_linenr 0-indexed.
-local function stdin_lines()
+---@return integer end_linenr 0-indexed. Already is exclusive.
+local function format_range()
     ---@type string
     local mode = vim.fn.mode()
-    local first_level_mode = mode:sub(1, 1)
+    local mode_char = mode:sub(1, 1)
 
-    -- If normal mode, return the entire buffer.
-    if first_level_mode == "n" then
-        local line_count = vim.api.nvim_buf_line_count(0)
-        return vim.api.nvim_buf_get_lines(0, 0, line_count, true), 0, line_count
+    -- If normal mode, range is the entire buffer.
+    if mode_char == "n" then
+        return 0, vim.api.nvim_buf_line_count(0)
     end
 
-    -- If visual mode, return only the selected lines.
-    if
-        first_level_mode == "v"
-        or first_level_mode == "V"
-        or first_level_mode == ""
-    then
+    -- If visual mode, range is the selected lines.
+    if mode_char == "v" or mode_char == "V" or mode_char == "" then
         local cursor_linenr = vim.fn.line(".")
         local visual_end_linenr = vim.fn.line("v")
-        -- Convert 1-indexed (line number) to 0-index.
+
+        -- Decrease by 1 to convert 1-indexed (line number) to 0-indexed.
         local start_linenr = math.min(cursor_linenr, visual_end_linenr) - 1
-        -- Exclusive, also converting 1-indexed to 0-indexed.
+        -- Should be exclusive, so we do not need to reduce by 1 here.
         local end_linenr = math.max(cursor_linenr, visual_end_linenr)
 
-        return vim.api.nvim_buf_get_lines(0, start_linenr, end_linenr, true),
-            start_linenr,
-            end_linenr
+        return start_linenr, end_linenr
     end
 
     -- Return nothing otherwise.
-    return {}, 0, 0
+    return 0, 0
+end
+
+---@param formatter fsr.formatter.Formatter.External
+local function format_with_external_command(formatter)
+    local buffer_path = vim.api.nvim_buf_get_name(0)
+
+    local command = { formatter.external.command }
+
+    for _, argument in ipairs(formatter.external.arguments or {}) do
+        argument = argument:gsub("{buffer_path}", buffer_path)
+        command[#command + 1] = argument
+    end
+
+    local start_linenr, end_linenr = format_range()
+    local stdin = vim.api.nvim_buf_get_lines(0, start_linenr, end_linenr, true)
+
+    local format_result = vim.system(command, {
+        text = true,
+        stdin = stdin,
+    }):wait()
+
+    if format_result.code ~= 0 then
+        vim.notify(format_result.stderr, vim.log.levels.ERROR)
+        return
+    end
+
+    local stdout = format_result.stdout
+    assert(type(stdout) == "string", "`stdout` should be text.")
+    ---@cast stdout string
+
+    -- `vim.system` normalize stdout line endings to `\n`.
+    local formatted_lines = vim.split(stdout, "\n", {
+        plain = true,
+        trimempty = true,
+    })
+
+    vim.api.nvim_buf_set_lines(
+        0,
+        start_linenr,
+        end_linenr,
+        true,
+        formatted_lines
+    )
+end
+
+---@param formatter fsr.formatter.Formatter.CodeAction
+local function format_with_code_action(formatter)
+    require("lsp.code_action").code_action_sync({
+        apply = true,
+        context = {
+            only = { formatter.code_action },
+        },
+    })
 end
 
 ---@param formatters fsr.formatter.Formatter[]?
@@ -56,59 +111,21 @@ function M.format(formatters)
         return
     end
 
-    local buffer_path = vim.api.nvim_buf_get_name(0)
-
     vim.iter(formatters):each(function(formatter)
+        ---@cast formatter fsr.formatter.Formatter
+
         if formatter.language_server then
-            vim.lsp.buf.format({
-                filter = function(client)
-                    return client.name == formatter.language_server.name
-                end,
-            })
+            format_with_language_server(
+                formatter --[[@as fsr.formatter.Formatter.LanguageServer]]
+            )
         elseif formatter.external then
-            local command = { formatter.external.command }
-
-            for _, argument in ipairs(formatter.external.arguments or {}) do
-                argument = argument:gsub("{buffer_path}", buffer_path)
-                command[#command + 1] = argument
-            end
-
-            local stdin, start_linenr, end_linenr = stdin_lines()
-
-            local sys_obj = vim.system(command, {
-                text = true,
-                stdin = stdin,
-            }):wait()
-
-            if sys_obj.code ~= 0 then
-                vim.notify(sys_obj.stderr, vim.log.levels.ERROR)
-                return
-            end
-
-            local stdout = sys_obj.stdout
-            assert(type(stdout) == "string", "`stdout` should be text.")
-            ---@cast stdout string
-
-            -- `vim.system` normalize stdout line endings to `\n`.
-            local stdout_lines = vim.split(stdout, "\n", {
-                plain = true,
-                trimempty = true,
-            })
-
-            vim.api.nvim_buf_set_lines(
-                0,
-                start_linenr,
-                end_linenr,
-                true,
-                stdout_lines
+            format_with_external_command(
+                formatter --[[@as fsr.formatter.Formatter.External]]
             )
         elseif formatter.code_action then
-            require("lsp.code_action").code_action_sync({
-                apply = true,
-                context = {
-                    only = { formatter.code_action },
-                },
-            })
+            format_with_code_action(
+                formatter --[[@as fsr.formatter.Formatter.CodeAction]]
+            )
         end
     end)
 end
