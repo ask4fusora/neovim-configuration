89a376d85960d7cc9d680a644d8c38bc04bd1a96
ask4fusora <theswe@ask4fusora.dev>
2026-09-13 21:43:02 +0700

    feat(lsp): add `TYPST_ROOT` configuration for `tinymist`

--- lsp/tinymist.lua
+++ lsp/tinymist.lua
@@ -5,18 +5,20 @@ return {
     root_markers = { ".git" },
     settings = {},
     before_init = function(params, config)
+        if type(vim.env.TYPST_ROOT) == "string" then
+            config.settings.rootPath =
+                vim.fs.abspath(vim.env.TYPST_ROOT --[[@as string]])
+        end
+
         if type(params.rootUri) == "string" then
-            config.settings = vim.tbl_deep_extend("force", config.settings, {
-                rootPath = vim.fs.abspath(
-                    vim.uri_to_fname(params.rootUri --[[@as string]])
-                ),
-            })
+            config.settings.rootPath = vim.fs.abspath(
+                vim.uri_to_fname(params.rootUri --[[@as string]])
+            )
         end
 
         if type(params.rootPath) == "string" then
-            config.settings = vim.tbl_deep_extend("force", config.settings, {
-                rootPath = vim.fs.abspath(params.rootPath --[[@as string]]),
-            })
+            config.settings.rootPath =
+                vim.fs.abspath(params.rootPath --[[@as string]])
         end
     end,
 }
