cfd3466fba58fccc0ac19c547d603b55b45bcab9
ask4fusora <theswe@ask4fusora.dev>
2026-09-13 16:17:12 +0700

    feat(lsp): configure root path for `tinymist`

--- lsp/tinymist.lua
+++ lsp/tinymist.lua
@@ -3,4 +3,20 @@ return {
     cmd = { "tinymist", "lsp" },
     filetypes = { "typst" },
     root_markers = { ".git" },
+    settings = {},
+    before_init = function(params, config)
+        if type(params.rootUri) == "string" then
+            config.settings = vim.tbl_deep_extend("force", config.settings, {
+                rootPath = vim.fs.abspath(
+                    vim.uri_to_fname(params.rootUri --[[@as string]])
+                ),
+            })
+        end
+
+        if type(params.rootPath) == "string" then
+            config.settings = vim.tbl_deep_extend("force", config.settings, {
+                rootPath = vim.fs.abspath(params.rootPath --[[@as string]]),
+            })
+        end
+    end,
 }
